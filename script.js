e4b0e676675546ab00f5d675075bcad74e7a7d1a
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-05-27 21:45:12 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,88 @@
+/* STICKY HEADER */
 
+const header =
+document.getElementById("header");
+
+window.addEventListener(
+"scroll",()=>{
+
+    header.classList.toggle(
+    "sticky",
+    window.scrollY > 50
+    );
+
+});
+
+/* MOBILE MENU */
+
+const toggle =
+document.getElementById(
+"menu-toggle"
+);
+
+const mobileMenu =
+document.getElementById(
+"mobile-menu"
+);
+
+toggle.addEventListener(
+"click",()=>{
+
+    mobileMenu.classList.toggle(
+    "active"
+    );
+
+});
+
+/* REVEAL */
+
+const reveals =
+document.querySelectorAll(
+".reveal"
+);
+
+window.addEventListener(
+"scroll",()=>{
+
+    reveals.forEach(reveal=>{
+
+        const windowHeight =
+        window.innerHeight;
+
+        const revealTop =
+        reveal.getBoundingClientRect().top;
+
+        if(revealTop <
+        windowHeight - 100){
+
+            reveal.classList.add(
+            "active"
+            );
+
+        }
+
+    });
+
+});
+
+/* PARALLAX */
+
+const parallax =
+document.querySelectorAll(
+".parallax"
+);
+
+window.addEventListener(
+"scroll",()=>{
+
+    let scroll =
+    window.pageYOffset;
+
+    parallax.forEach(img=>{
+
+        img.style.transform =
+        `translateY(${scroll * 0.12}px)`;
+
+    });
+
+});
